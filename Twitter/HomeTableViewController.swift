494fe162dbee70bcffdd5b0c46d4c3699e5d08ab
maxskeen <69686704+maxskeen@users.noreply.github.com>
2021-03-06 21:29:16 -0500

    test

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -21,7 +21,7 @@ class HomeTableViewController: UITableViewController {
     @objc func loadTweet() {
         
         numberOfTweet = 20
-        
+        //modifying
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         
         let myParams = ["count": numberOfTweet]
